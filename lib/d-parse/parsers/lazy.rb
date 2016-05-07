cdfef5d9abc321f3aa487846fa21ba2304b3be2d
Denis Defreyne <denis.defreyne@stoneship.org>
2016-05-07 10:03:01 +0200

    Move Whitespace and Lazy into lib

---
+++ lib/d-parse/parsers/lazy.rb
@@ -0,0 +1,18 @@
+module DParse
+  module Parsers
+    class Lazy < DParse::Parser
+      def initialize(&block)
+        raise ArgumentError, 'Expected block' unless block_given?
+        @block = block
+      end
+
+      def read(input, pos)
+        @block.call.read(input, pos)
+      end
+
+      def inspect
+        'lazy(?)'
+      end
+    end
+  end
+end
---
+++ lib/d-parse/parsers/whitespace.rb
@@ -0,0 +1,16 @@
+module DParse
+  module Parsers
+    class Whitespace < DParse::Parser
+      WS = [' ', "\t"].freeze
+
+      def read(input, pos)
+        loop do
+          break unless WS.include?(input[pos.index])
+          pos = pos.advance
+        end
+
+        Success.new(pos)
+      end
+    end
+  end
+end
