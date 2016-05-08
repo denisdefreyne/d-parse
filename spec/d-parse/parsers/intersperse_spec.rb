098b607150948df71c0cac679446d01032745768
Denis Defreyne <denis.defreyne@stoneship.org>
2016-05-08 13:05:30 +0200

    Add spec for Intersperse

---
+++ spec/d-parse/parsers/intersperse_spec.rb
@@ -0,0 +1,18 @@
+describe DParse::Parsers::Intersperse do
+  let(:a) { DParse::Parsers::Char.new('a') }
+  let(:b) { DParse::Parsers::Char.new(',') }
+  let(:parser) { described_class.new(a, b) }
+
+  example { expect(parser).to parse('a').up_to(1) }
+  example { expect(parser).to parse('a,a').up_to(3) }
+  example { expect(parser).to parse('a,a,a').up_to(5) }
+
+  example { expect(parser).to parse('a,').up_to(1) }
+  example { expect(parser).to parse('a,a,').up_to(3) }
+
+  example { expect(parser).not_to parse('') }
+  example { expect(parser).not_to parse(',') }
+  example { expect(parser).not_to parse(',a') }
+  example { expect(parser).not_to parse(',a,') }
+  example { expect(parser).not_to parse(',a,a') }
+end
