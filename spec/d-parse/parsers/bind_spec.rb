6a7537ba80fb71b4ad72eb63ca73bf4caec452ed
Denis Defreyne <denis.defreyne@stoneship.org>
2016-05-08 13:09:29 +0200

    Add spec for Bind

---
+++ spec/d-parse/parsers/bind_spec.rb
@@ -0,0 +1,31 @@
+describe DParse::Parsers::Bind do
+  let(:a) { DParse::Parsers::Char.new('a') }
+  let(:b) { DParse::Parsers::Char.new('b') }
+
+  let(:init) { (a | b).capture }
+
+  let(:parser) do
+    parser_map = {
+      'a' => DParse::Parsers::Char.new('0'),
+      'b' => DParse::Parsers::Char.new('1'),
+    }
+
+    init.bind do |data|
+      new_parser = parser_map[data]
+      new_parser.map { |d| [data, d] }
+    end
+  end
+
+  example { expect(parser).to parse('a0').up_to(2) }
+  example { expect(parser).to parse('b1').up_to(2) }
+
+  example { expect(parser).not_to parse('') }
+  example { expect(parser).not_to parse('a') }
+  example { expect(parser).not_to parse('aa') }
+  example { expect(parser).not_to parse('ab') }
+  example { expect(parser).not_to parse('a1') }
+  example { expect(parser).not_to parse('b') }
+  example { expect(parser).not_to parse('ba') }
+  example { expect(parser).not_to parse('bb') }
+  example { expect(parser).not_to parse('b0') }
+end
