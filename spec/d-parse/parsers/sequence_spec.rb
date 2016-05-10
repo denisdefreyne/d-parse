1ba7b3b707f5740f4f7cba39a34e39db582b3eb5
Denis Defreyne <denis.defreyne@stoneship.org>
2016-05-10 22:06:48 +0200

    Test failures of sequence

--- spec/d-parse/parsers/sequence_spec.rb
+++ spec/d-parse/parsers/sequence_spec.rb
@@ -3,10 +3,11 @@ describe DParse::Parsers::Sequence do
   let(:char_b) { DParse::Parsers::Char.new('b') }
   let(:parser) { described_class.new(char_a, char_b) }
 
-  example { expect(parser).not_to parse('') }
-  example { expect(parser).not_to parse('a') }
-  example { expect(parser).not_to parse('b') }
-  example { expect(parser).not_to parse('ba') }
+  example { expect(parser).not_to parse('').with_failure('expected \'a\', but found end of input at line 1, column 1') }
+  example { expect(parser).not_to parse('a').with_failure('expected \'b\', but found end of input at line 1, column 2') }
+  example { expect(parser).not_to parse('ac').with_failure('expected \'b\', but found \'c\' at line 1, column 2') }
+  example { expect(parser).not_to parse('b').with_failure('expected \'a\', but found \'b\' at line 1, column 1') }
+  example { expect(parser).not_to parse('ba').with_failure('expected \'a\', but found \'b\' at line 1, column 1') }
 
   example { expect(parser).to parse('ab').up_to(2) }
   example { expect(parser).to parse('aba').up_to(2) }
