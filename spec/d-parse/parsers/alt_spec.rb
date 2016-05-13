86bc46d1ffa8a9b155748ce6581112ee0ec40421
Denis Defreyne <denis@soundcloud.com>
2016-05-13 11:04:15 +0200

    wip

---
+++ spec/d-parse/parsers/alt_spec.rb
@@ -0,0 +1,12 @@
+describe DParse::Parsers::Alt do
+  let(:a) { DParse::Parsers::String.new('Hello') }
+  let(:b) { DParse::Parsers::String.new('Goodbye') }
+  let(:parser) { described_class.new(a, b) }
+
+  example { expect(parser).to parse('Hello').up_to(5) }
+  example { expect(parser).to parse('Goodbye').up_to(7) }
+
+  example { expect(parser).not_to parse('').and_fail_at(0) }
+  example { expect(parser).not_to parse('Wilkommen').and_fail_at(0) }
+  example { expect(parser).not_to parse('Hallo').and_fail_at(0) }
+end
