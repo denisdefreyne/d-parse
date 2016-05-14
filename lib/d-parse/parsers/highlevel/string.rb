b766b098f76aaa069e16afa8af4fa537af622da3
Denis Defreyne <denis.defreyne@stoneship.org>
2016-05-14 14:44:03 +0200

    feat(parsers): Convert String parser to high-level parser

---
+++ lib/d-parse/parsers/highlevel/string.rb
@@ -0,0 +1,13 @@
+module DParse
+  module Parsers
+    class String < DParse::Parser
+      def self.new(string)
+        DParse::Parsers::Seq.new(*string.chars.map { |c| DParse::Parsers::Char.new(c) })
+      end
+
+      def initialize(*)
+        raise ArgumentError, "#{self.class} is not supposed to be initialized"
+      end
+    end
+  end
+end
