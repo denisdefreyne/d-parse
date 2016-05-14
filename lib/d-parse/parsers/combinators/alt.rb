d4a20ab7f9c0f696d284d50690a23854f35ccd09
Denis Defreyne <denis.defreyne@stoneship.org>
2016-05-14 15:50:14 +0200

    feat(parsers): Let Alt return most specific failure

--- lib/d-parse/parsers/combinators/alt.rb
+++ lib/d-parse/parsers/combinators/alt.rb
@@ -2,19 +2,36 @@ module DParse
   module Parsers
     class Alt < DParse::Parser
       def initialize(*parsers)
+        # FIXME: ensure >0 parsers are provided
         @parsers = parsers
       end
 
       def read(input, pos)
         @parsers
-          .lazy
           .map { |parser| parser.read(input, pos) }
-          .select { |res| res.is_a?(Success) }
-          .first || Failure.new(input, pos)
+          .reduce { |a, e| best(a, e) }
       end
 
       def inspect
         "alt(#{@parsers.map(&:inspect).join(',')})"
+      end
+
+      def best(a, b)
+        case a
+        when DParse::Success
+          a
+        when DParse::Failure
+          case b
+          when DParse::Success
+            b
+          when DParse::Failure
+            if a.pos.index > b.pos.index
+              a
+            else
+              b
+            end
+          end
+        end
       end
     end
   end
