d18168d10e2e8d18b3b973d92db2d1782be5842b
Denis Defreyne <denis.defreyne@stoneship.org>
2016-05-14 12:49:10 +0200

    refactor(parsers): Sort methods in DSL

--- lib/d-parse/dsl.rb
+++ lib/d-parse/dsl.rb
@@ -1,5 +1,9 @@
 module DParse
   module DSL
+    def alt(*ps)
+      DParse::Parsers::Alt.new(*ps)
+    end
+
     def char(c)
       DParse::Parsers::Char.new(c)
     end
@@ -16,36 +20,32 @@ module DParse
       DParse::Parsers::CharNotIn.new(cs)
     end
 
+    def end_of_input
+      DParse::Parsers::EndOfInput.new
+    end
+
     def intersperse(a, b)
       DParse::Parsers::Intersperse.new(a, b)
     end
 
-    def repeat(p)
-      DParse::Parsers::Repeat.new(p)
+    def lazy(&block)
+      DParse::Parsers::Lazy.new(&block)
     end
 
     def opt(p)
       DParse::Parsers::Optional.new(p)
     end
 
+    def repeat(p)
+      DParse::Parsers::Repeat.new(p)
+    end
+
     def seq(*ps)
       DParse::Parsers::Sequence.new(*ps)
     end
 
-    def alt(*ps)
-      DParse::Parsers::Alt.new(*ps)
-    end
-
     def string(s)
       DParse::Parsers::String.new(s)
-    end
-
-    def end_of_input
-      DParse::Parsers::EndOfInput.new
-    end
-
-    def lazy(&block)
-      DParse::Parsers::Lazy.new(&block)
     end
 
     def whitespace_char
