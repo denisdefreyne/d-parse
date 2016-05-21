1a554b4235ecc02c21f6e5d4a978de3628cfc331
Denis Defreyne <denis.defreyne@stoneship.org>
2016-05-21 16:50:13 +0200

    fix(core): Fix newlines in failure message

--- lib/d-parse/failure.rb
+++ lib/d-parse/failure.rb
@@ -19,12 +19,12 @@ module DParse
     end
 
     def pretty_message
-      line = input.lines[@pos.line] || ''
+      line = (input.lines[@pos.line] || '').rstrip
       fancy_line = line.chars.map.with_index { |c, i| i == @pos.column ? "\e[31m" + c + "\e[0m" : c }.join
 
       lines = [full_message, '', fancy_line, "\e[31m" + ' ' * @pos.column + '↑' + "\e[0m"]
 
-      lines.map { |l| "#{l.strip}\n" }.join('')
+      lines.join("\n")
     end
 
     def to_s
--- spec/d-parse/failure_spec.rb
+++ spec/d-parse/failure_spec.rb
@@ -11,6 +11,20 @@ describe DParse::Failure do
 
   describe '#pretty_message' do
     subject { failure.pretty_message }
-    it { is_expected.to eql("argh at line 3, column 4\n\nthr\e[31me\e[0me\n\e[31m   ↑\e[0m\n") }
+
+    context 'column 3' do
+      let(:position) { DParse::Position.new(index: 13, line: 2, column: 3) }
+      it { is_expected.to eql("argh at line 3, column 4\n\nthr\e[31me\e[0me\n\e[31m   ↑\e[0m") }
+    end
+
+    context 'column 4' do
+      let(:position) { DParse::Position.new(index: 14, line: 2, column: 4) }
+      it { is_expected.to eql("argh at line 3, column 5\n\nthre\e[31me\e[0m\n\e[31m    ↑\e[0m") }
+    end
+
+    context 'column 5' do
+      let(:position) { DParse::Position.new(index: 15, line: 2, column: 5) }
+      it { is_expected.to eql("argh at line 3, column 6\n\nthree\n\e[31m     ↑\e[0m") }
+    end
   end
 end
