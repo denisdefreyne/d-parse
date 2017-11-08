2fab9de566f10ed8a32d9c7da40b047df8d71b7c
Denis Defreyne <denis.defreyne@stoneship.org>
2017-11-08 21:29:40 +0100

    Test DSL#char

--- spec/d-parse/dsl_spec.rb
+++ spec/d-parse/dsl_spec.rb
@@ -10,4 +10,14 @@ describe DParse::DSL do
     it { is_expected.to parse('é') }
     it { is_expected.not_to parse('') }
   end
+
+  describe '.char' do
+    include described_class
+
+    subject { char('x') }
+
+    it { is_expected.to parse('x') }
+    it { is_expected.not_to parse('é') }
+    it { is_expected.not_to parse('') }
+  end
 end
