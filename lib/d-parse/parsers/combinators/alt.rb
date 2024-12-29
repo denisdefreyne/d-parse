73ba145f6edb6c12e289caea208ec0c570368a67
gemmaro <gemmaro.dev@gmail.com>
2024-12-29 16:02:39 +0900

    Refactor alternative combinator's read method

--- lib/d-parse/parsers/combinators/alt.rb
+++ lib/d-parse/parsers/combinators/alt.rb
@@ -11,17 +11,12 @@ module DParse
       def read(input, pos)
         init = DParse::Failure.new(input, DParse::Position::FAR_BEHIND)
         @parsers.reduce(init) do |old_res, parser|
-          case old_res
+          new_res = parser.read(input, pos)
+          case new_res
           when DParse::Success
-            old_res
+            return new_res.with_best_failure(old_res)
           when DParse::Failure
-            new_res = parser.read(input, pos)
-            case new_res
-            when DParse::Success
-              new_res.with_best_failure(old_res)
-            when DParse::Failure
-              [old_res, new_res].max_by { |r| r.pos.index }
-            end
+            [old_res, new_res].max_by { |r| r.pos.index }
           end
         end
       end
